8d74daff050146a918dfba761c80aa002c2d54c8
Christ ASSANA <christassana@gmail.com>
2024-06-07 15:40:13 +0200

    Ajoutez des fichiers projet.

---
+++ projTableauxMulti/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class MainProgram
+{
+    public static void Main(string[] args)
+    {
+        
+        int[,] ArrayUniforme = new int[3, 3];
+        ArrayUniforme = new int[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 5 } };
+        int[][] jaggedArray = new int[3][];
+
+       
+        jaggedArray[0] = new int[3] {1,2,3};
+        jaggedArray[1] = new int[2] {3,4};
+        jaggedArray[2] = new int[1] {5};
+
+        for (int i = 0; i < jaggedArray.Length; i++)
+        {
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+            {
+                Console.Write(jaggedArray[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
