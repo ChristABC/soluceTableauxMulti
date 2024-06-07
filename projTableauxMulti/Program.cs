23f9fbbb8a837566c7c23a4d77e6de26ab4ec642
Christ ASSANA <christassana@gmail.com>
2024-06-07 15:54:34 +0200

    Update 2

--- projTableauxMulti/Program.cs
+++ projTableauxMulti/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.Data;
 class MainProgram
 {
     public static void Main(string[] args)
     {
         
-        int[,] ArrayUniforme = new int[3, 3];
-        ArrayUniforme = new int[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 5 } };
+        int[,] arrayUniforme = new int[3, 3];
+        arrayUniforme = new int[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 5 } };
         int[][] jaggedArray = new int[3][];
+        int a = arrayUniforme[0,0];
+        int b = arrayUniforme[0,1];
+        int c = arrayUniforme[0,2];
+        int d = arrayUniforme[1,2];
+        int e = arrayUniforme[2,2];
 
-       
-        jaggedArray[0] = new int[3] {1,2,3};
-        jaggedArray[1] = new int[2] {3,4};
-        jaggedArray[2] = new int[1] {5};
+
+
+        jaggedArray[0] = new int[3] {a,b,c};
+        jaggedArray[1] = new int[2] {c,d};
+        jaggedArray[2] = new int[1] {e};
 
         for (int i = 0; i < jaggedArray.Length; i++)
         {
